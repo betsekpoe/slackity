942a37aaa2724e5988540839589626358c2c69db
Tsekpoe Blessing Eyram <betsekpoe@st.knust.edu.gh>
2026-05-14 02:02:07 +0000

    Configure tailwind, shadcn, firebase

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login-form"
+
+export default function LoginPage() {
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<LoginForm className="w-full max-w-md mx-auto"/>
+		</div>
+	)
+}
